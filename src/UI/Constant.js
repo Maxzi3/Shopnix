83fbfbc2a32b78e37676a9f7144e5f9fb3648da8
Maxzi3 <obisikemaxwell@gmail.com>
2025-05-11 06:21:10 +0100

    updated the backend url

--- src/UI/Constant.js
+++ src/UI/Constant.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:3000/api/v1";
+export const API_BASE_URL = "https://shopnix-backend.onrender.com/api/v1";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
